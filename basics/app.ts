d477bd69f89b27413cfcf2bda7684e9d2d0ba4d1
Rakshat28 <rakshat07ps@gmail.com>
2024-12-30 19:08:14 +0530

    ts setup , compiler , config , and tsc --watch

---
+++ basics/app.ts
@@ -0,0 +1,12 @@
+var a = 12;
+
+// when you want to compile , just write in the terminal => tsc app.ts, 
+//  and you will get a JS equivalent of the ts code that you wrote
+
+
+// to create a ts config file jus write in the terminal :: tsc --init and you will get 
+// tscongif.json
+
+
+// if you wirte tsc --watch int the terminal , then whenever you save your ts code ,
+// it will automatically be converted to js equivalent
